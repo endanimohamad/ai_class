1d3ab0577bdaeccfc05b792459654cdfd3ee5d0c
endanimohamad <mohamad.university3@gmail.com>
2025-07-22 20:49:09 +0330

    31/4/1404

---
+++ pandas_practice.py
@@ -0,0 +1,23 @@
+import os
+import ast
+import pandas as pd
+
+def read_py_file():
+    dpath = os.path.join("D:\\", "bank_customer.txt")
+    with open(dpath, 'r', encoding="utf-8") as file:
+        content = file.read()
+    dataset = ast.literal_eval(content)
+    df = pd.DataFrame(dataset)
+    return df
+
+df = read_py_file()
+# اعمال شرط با پاندا
+eligible = df[
+    (df['age'] >= 25) &
+    (df['age'] <= 60) &
+    (df['work_experience'] >= 2)
+]
+
+# نمایش واجدین شرایط
+print("افراد واجد شرایط دریافت وام:\n")
+print(eligible[['name', 'last_name']].to_string())
