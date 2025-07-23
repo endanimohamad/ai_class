53cfc11ab576cbc0f22b2b73f827e084bcc396c5
endanimohamad <mohamad.university3@gmail.com>
2025-07-23 20:25:40 +0330

    Update pandas_practice.py

--- pandas_practice.py
+++ pandas_practice.py
@@ -6,8 +6,8 @@ def read_py_file():
     dpath = os.path.join("D:\\", "bank_customer.txt")
     with open(dpath, 'r', encoding="utf-8") as file:
         content = file.read()
-    dataset = ast.literal_eval(content)
-    df = pd.DataFrame(dataset)
+        dataset = ast.literal_eval(content)
+        df = pd.DataFrame(dataset)
     return df
 
 df = read_py_file()
