e5f9e758dafe8f20203c5860840810bcca7e8d49
endanimohamad <mohamad.university3@gmail.com>
2025-07-25 03:22:34 +0330

    Update pandas_practice2.py

--- pandas_practice2.py
+++ pandas_practice2.py
@@ -32,12 +32,37 @@ def conditions():
 
   together_condition = df[condition_age_income & condition_gender & condition_home]
 
-  # ترتیب سن صعودی برای جوانگرایی
   sort_age_people = together_condition.sort_values(by= 'age')
 
   return sort_age_people[['name', 'last_name', 'age', 'income']]
 
-# پرینت افراد واجد شرایط وام با در نظر گرفتن اولویت سن از کم به زیاد
-print(conditions())
+print(f' \n                                                                                      :افراد واجد شرایط دریافت وام{conditions()}')
+
+
+def calculation():
+  age_mean = df['age'].mean()
+  print(f' : میانگین سنی افراد متقاضی وام: {age_mean}')
+  income_mean = df['income'].mean()
+  print(f' : میانگین درامد افراد متقاضی وام: {income_mean}')
+  print("\n")
+
+  age_median = df['age'].median()
+  print(f' : میانه(تقریبا متوسط) سنی افراد متقاضی وام: {age_median}')
+  income_median = df['income'].median()
+  print(f' : میانه(تقریبا متوسط) درامدی افراد متقاضی وام: {income_median}')
+  print("\n")
+  age_mode = df['age'].mode().iloc[0]
+  print(f' نمای (بیشترین) سن پرتکرا افراد متقاضی وام: {age_mode}')
+
+  name_mode = df['name'].mode().iloc[0]
+  print(f' : نمای (بیشترین) اسم پرتکرا افراد متقاضی وام: {name_mode}')
+
+  car_mode = df['car'].mode().iloc[0]
+  print(f'  نمای (بیشترین) ماشین پرتکرا افراد متقاضی وام: {car_mode}')
+
+  education_mode = df['education'].mode().iloc[0]
+  print(f'  نمای (بیشترین) مدرک تحصیلی پرتکرا افراد متقاضی وام: {education_mode}')
+
+calculation()
 
 
